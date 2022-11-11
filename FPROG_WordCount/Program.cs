c6b19d2eaddf8003e264d3553d05d9dc2199509b
sunny_trashpanda <lucienne.24.angeli@gmail.com>
2022-11-11 12:11:49 +0100

    Started Project

--- FPROG_WordCount/Program.cs
+++ FPROG_WordCount/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FPROG_WordCount   
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            //pfad angeben, auslesen
+            //dateiendung auslesen
+            string dirPath = "help";
+            string fileExtension = "help";
+
+            //auslagern dann!
+            var files = Directory.EnumerateFiles(dirPath, "*" + fileExtension, SearchOption.AllDirectories);
+            IEnumerable<string>  fileList = Directory.EnumerateFiles(dirPath, "*" + fileExtension, SearchOption.AllDirectories);
+
+            Console.WriteLine("Sewas!");
+
+
+
+        }
+
+
+        // kopiert von stack overflow --> ' and "
+        public static string RemoveSpecialCharacters(string input)
+        {
+            Regex r = new Regex(
+                          "(?:[^a-zA-Z0-9 ]|(?<=['\"])s)",
+                          RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+            return r.Replace(input, String.Empty);
+        }
+    }
+}
