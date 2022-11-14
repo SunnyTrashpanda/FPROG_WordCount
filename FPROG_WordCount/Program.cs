aa071fe695c6bb3989a5a658453d02f3c3ff692b
MarliesR <marlies.rieder95@gmail.com>
2022-11-14 18:02:27 +0100

    map reduce, delimiters and get files from directory functional

--- FPROG_WordCount/Program.cs
+++ FPROG_WordCount/Program.cs
@@ -1,73 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 
 namespace FPROG_WordCount   
 {
-    internal class Program
+    internal static class Program
     {
 
 
-       
+
         //https://stackoverflow.com/questions/428798/map-and-reduce-in-net
 
         //create higher order function for mapping 
 
         //aus reflecting functional power point 
 
-       //MAPPING single values to many
-        static IEnumerable<TResult> SelectMany<TSource, TResult>(this<TSource> source, Func<TSource, IEnumerable<TResult>> selector)
-        {
-            var files = new List<TResult>();
-            //select many
-            return File.ReadLines(source).SelectMany(line => line.Split(delimiters));
+        //https://weblogs.asp.net/dixin/functional-csharp-higher-order-function-currying-and-first-class-function
+        //https://www.codeproject.com/Articles/375166/Functional-Programming-in-Csharp#FunctionTypes
 
-            return files;
+
+
+        public static IEnumerable<TResult> MapReduce<TSource, TMapped, TKey, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, IEnumerable<TMapped>> map,
+            Func<TMapped, TKey> keySelector,
+            Func<IGrouping<TKey, TMapped>, IEnumerable<TResult>> reduce)
+        {
+            return source.SelectMany(map).GroupBy(keySelector).SelectMany(reduce);
         }
 
-        //GROUP elements by some key
-        static IEnumerable<IGrouping<TKey, TSource>> GroupBy<TSource,TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        public static Func<string, string, IEnumerable<string>> GetFilesListFromDirectory = (dirPath, fileExtension) =>
+         {
+             return Directory.EnumerateFiles(dirPath, "*" + fileExtension, SearchOption.AllDirectories);
+         };
+
+        public static void printWords(IEnumerable<KeyValuePair<string, int>> wordlist)
         {
+            foreach (var c in wordlist.OrderByDescending(x => x.Value))
+            {
+                Console.Write($"{c.Key} -> {c.Value}____");
+            }
 
         }
 
-        //reduce 
+        
 
-       
-
-
-        //First class function
-        Func<int, int> square = x =>
-        {
-            return x * x;
-        };
-
-
-
+        
+        //DISCLAIMER: muss noch alles überprüft werden ob funktional ! 
         static void Main(string[] args)
         {
-            //pfad angeben, auslesen
-            //dateiendung auslesen
-            string dirPath = "help";
-            string fileExtension = "help";
+            //string dirPath = @"C:\Users\Marlies\source\repos\SunnyTrashpanda\FPROG_WordCount\testfiles";
+            //string fileExtension = ".txt";
 
-            //auslagern dann!
-            var files = Directory.EnumerateFiles(dirPath, "*" + fileExtension, SearchOption.AllDirectories);
+            string dirPath;
+            string fileExtension;
 
-            //
-            var counts = files.MapReduce(Path => File.ReadLines(path).SelectMany(line => line.Split(delimiters)),
-                                         word => word,
-                                         group => new[] { new KeyValuePair<string, int>(group.key, group.Count()) });
+            if (Environment.GetCommandLineArgs().Length > 2)
+            {
+                char[] delimiters = Enumerable.Range(0, 256).Select(i => (char)i).Where(c => Char.IsWhiteSpace(c) || Char.IsPunctuation(c)).ToArray();
 
-            
-           
+                dirPath = Environment.GetCommandLineArgs()[1];
+                //TODO wenn keine file extension mitgegeben wird
+                fileExtension = Environment.GetCommandLineArgs()[2];
 
+                var files = GetFilesListFromDirectory(dirPath, fileExtension);
 
+                var counts = MapReduce(files, //source
+                                        path => File.ReadLines(path).SelectMany(line => line.Split(delimiters)), //map 
+                                        word => word, //key
+                                                      //word => RemoveSpecialChars(word), //key
+                                        group => new[] { new KeyValuePair<string, int>(group.Key, group.Count()) }); ; //reduce
+
+                printWords(counts);
+            }
 
         }
 
 
+        //NOT NEEDED
         // kopiert von stack overflow --> ' and "
         public static string RemoveSpecialCharacters(string input)
         {
@@ -76,5 +90,16 @@ namespace FPROG_WordCount
                           RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
             return r.Replace(input, String.Empty);
         }
+
+        //braucht man eigentlich nicht, durch die delimiters schon erledigt 
+        public static Func<string, string> RemoveSpecialChars = (input) =>
+        {
+            Regex r = new Regex(
+                          "(?:[^a-zA-Z0-9 ]|(?<=['\"])s)",
+                          RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+            return r.Replace(input, String.Empty);
+        };
+
+
     }
 }
