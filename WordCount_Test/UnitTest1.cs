7598594da471b5bd5faa7f11fb3b9c957190f541
MarliesR <marlies.rieder95@gmail.com>
2022-12-04 11:55:10 +0100

    first Unit Test

--- WordCount_Test/UnitTest1.cs
+++ WordCount_Test/UnitTest1.cs
@@ -1,3 +1,6 @@
+using FPROG_WordCount;
+using static System.Net.Mime.MediaTypeNames;
+
 namespace WordCount_Test
 {
     public class Tests
@@ -5,12 +8,38 @@ namespace WordCount_Test
         [SetUp]
         public void Setup()
         {
+            
         }
 
         [Test]
-        public void Test1()
+        public void SmallTestFile()
         {
-            Assert.Pass();
+            var enviroment = System.Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(enviroment).Parent.Parent.Parent.FullName;
+
+            string path = projectDirectory + "\\testfiles\\test";
+            string fileExtension = ".txt";
+
+            var files = Program.GetFilesListFromDirectory(path, fileExtension);
+
+            var delimiters = Program.GetDelimiters();
+
+            var counts = Program.MapReduce(files,
+                                    path => File.ReadLines(path).SelectMany(line => line.Split(delimiters)).Where(line => !string.IsNullOrWhiteSpace(line)),  
+                                    word => word, 
+                                    group => new[] { new KeyValuePair<string, int>(group.Key, group.Count()) }); 
+
+            var orderedCounts = counts.OrderByDescending(x => x.Value).ThenByDescending(x => x.Key);
+
+            IEnumerable<KeyValuePair<string, int>> result = new List<KeyValuePair<string, int>> { 
+                new KeyValuePair<string, int> ("hallo",3),
+                new KeyValuePair<string, int> ("hop",2),
+                new KeyValuePair<string, int> ("hi",1),
+                new KeyValuePair<string, int> ("help",1),
+            };
+
+            Assert.That(result, Is.EqualTo(orderedCounts));
+            
         }
     }
 }
